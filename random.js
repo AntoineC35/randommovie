d94ed9dbbee3373e988ac97ef8b747d16dd55219
AntoineC35 <antoine.cormier@3wa.io>
2023-12-15 14:39:23 +0000

    feat(random.js) ajout de composant

--- random.js
+++ random.js
@@ -2,6 +2,7 @@ const keyTMDB = "cc4deeed666ddf53c6829e7e89af8f69";
 const tokenTMDB = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzRkZWVlZDY2NmRkZjUzYzY4MjllN2U4OWFmOGY2OSIsInN1YiI6IjY0NzcyOTVlMTJjNjA0MDBlMWRjOGViNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WzioNlWeD9igUGz2pYXEDO1tHKj4qki0U6HKRPYwvQs"
 const URLIMDB = ""
 const main = document.querySelector(".container")
+let searchResult;
 
 
 const options = {
@@ -12,24 +13,60 @@ const options = {
   }
 };
 
-fetch('https://api.themoviedb.org/3/authentication', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-  
-fetch('https://api.themoviedb.org/3/genre/movie/list', options)
+function fetchGenre() {
+  fetch('https://api.themoviedb.org/3/genre/movie/list', options)
     .then(response => response.json())
     .then(data => createList(data.genres))
     .catch(err => console.error(err));
-    
+}
+
 function createList(data) {
-    const select = document.querySelector(".genreSelector") || document.createElement("select")
-    select.classList.add("select")
-    data.forEach(function(genre) {
-        const optionGenre = document.createElement("option");
-        optionGenre.classList.add("genre")
-        optionGenre.innerHTML = genre.name
-        select.appendChild(optionGenre);
+  const fSearchEngine = document.querySelector(".formSearch")
+  const select = document.querySelector(".select")
+  
+  data.forEach(function(genre) {
+    const optionGenre = document.createElement("option");
+    optionGenre.classList.add("genre")
+    optionGenre.id = genre.id
+    optionGenre.value = genre.name
+    optionGenre.innerHTML = genre.name
+    select.appendChild(optionGenre);
+  })
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetchGenre();
+  const btnSearch = document.querySelector(".btnSearch");
+  const fSearchEngine = document.querySelector(".formSearch");
+
+  btnSearch.addEventListener("click", function(event) {
+    event.preventDefault();
+    const formData = new FormData(fSearchEngine);
+    const search = formData.get("genres");
+    searchResult = search
+    getMovieinfo()
+  });
+});
+
+
+
+function getMovieinfo() {
+  fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${searchResult}`, options)
+    .then(response => response.json())
+    .then(function(data) {
+      randomMovieData(data.results);
     })
-    main.appendChild(select)
+    .catch(err => console.error(err));
+}
+
+function randomMovieData(data) {
+  let randomIndex = Math.floor(Math.random() * 20);
+  const movieTitle = data[randomIndex].title;
+  const movieSynopsis = data[randomIndex].overview;
+  const posterMovie = `https://image.tmdb.org/t/p/original${data[randomIndex].posters_path}`
+}
+
+function createCards(movieTitle, movieSynopsis, posterMovie) {
+  const cardRandom = document.querySelector(".cardRandom") || document.createElement("article")
+  cardRandom.classList.add("")
 }
